8d517f336280d00b3ee7e9a7814a8686b625cc13
Robert Statsinger <rstats@pacbell.net>
2018-10-12 16:18:21 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # cp /var/jenkins_home/workspace/ShiftLeftDeveloperDemo/shiftleft.json.
 
-sl run -- java -jar /var/jenkins_home/workspace/ShiftLeftDeveloperDemo/target/hello-shiftleft-0.0.1.jar &
+sl run -- java -jar /var/jenkins_home/workspace/ShiftLeftDeveloperDemo/target/hello-shiftleft-0.0.1.jar --server.port=3030 &
 
 sleep 60
 while true ; do \
