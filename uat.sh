0c2f43c3c4d29b2ba519c978adebc9cb1a5a3f5e
Robert Statsinger <rstats@pacbell.net>
2018-10-16 11:14:34 -0700

    Update uat.sh

--- uat.sh
+++ uat.sh
@@ -25,10 +25,15 @@ curl -s localhost:3030/patients >/dev/null ;\
 curl -s localhost:3030/patients >/dev/null ;\
 curl -s localhost:3030/patients >/dev/null ;\
 
+sleep 5 ;\
+
 wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >&/dev/null ;\
+wget "localhost:3030/search/user?foo=new java.lang.ProcessBuilder({'/bin/bash','-c','echo 3vilhax0r>/tmp/hacked'}).start()" >&/dev/null ;\
+
+sleep 5 ;\
 
 wget "localhost:3030/debug?customerId=1&clientId=1&firstName=a&lastName=b&dateOfBirth=123&ssn=123&socialSecurityNum=1&tin=123&phoneNumber=5432%3Cscriscriptpt%3Ealert(1)%3C/sscriptcript%3E" >&/dev/null ;\
 
 /bin/rm -f ./debug*
-sleep 4;
+sleep 4 ;\
 done
